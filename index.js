6609eb22ac76e66af06aeeb515fa07a5ce0aad98
Daniel Moraes <moraesdeveloper@gmail.com>
2020-09-28 02:18:30 -0300

    Altered response method

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ validaCEPGotham.post("/zipcode", async (req, res) => {
     var {zipCode, city} = req.body;
 
     if (!(zipCodeController.formatIsValid(zipCode))) {
-        return res.json({error: "O formato do CEP é inválido"});
+        return res.send({error: "O formato do CEP é inválido"});
     }
 
     if (!(zipCodeController.rangeIsValid(zipCode))) {
-        return res.json({error: "O CEP deve ser um número maior que " + zipCodeController.minRange + " e menor que " + zipCodeController.maxRange});
+        return res.send({error: "O CEP deve ser um número maior que " + zipCodeController.minRange + " e menor que " + zipCodeController.maxRange});
     }
 
     await zipCodeController.findOne(zipCode, (error, result) => {
-        if (error) { return res.json({error})};
+        if (error) { return res.send({error})};
 
         if (result.length > 0) {
-            return res.json({error: "Esse CEP já está cadastrado para a cidade de " + result[0].city});
+            return res.send({error: "Esse CEP já está cadastrado para a cidade de " + result[0].city});
         }
     });
 
     zipCodeController.add({zipCode, city}, (error, result) => {
-        if (error) { return res.json({error}); }
+        if (error) { return res.send({error}); }
 
-        res.json({msg: "CEP inserido com sucesso!"});
+        res.send({msg: "CEP inserido com sucesso!"});
     });
 });
